721daefd3c535ee694d3d69769214640bdf1e104
Ruben Allenstein <ru.allenstein@reply.de>
2022-12-09 10:15:21 +0100

    [ADD] adding new tracking code to order via api request

---
+++ setTrackingcode.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App;
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use function json_encode;
+
+const USERNAME = 'p574147';
+const PASSWORD = 'iZba8#e+w4zflh';
+const HOST = 'http://shopware.p574147.webspaceconfig.de';
+
+$client = new Client();
+$accessToken = getAccessToken($client);
+
+$request = new Request(
+    'GET',
+    HOST . '/api/order/6e96bb30203e4355b6a10caeec40c1b3',
+    [
+        'Authorization' => 'Bearer ' . $accessToken,
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+    ]
+);
+$response = $client->send($request);
+$body = json_decode($response->getBody()->getContents(), true);
+
+function getAccessToken($client)
+{
+    $body = json_encode([
+        'client_id' => 'administration',
+        'grant_type' => 'password',
+        'scopes' => '',
+        'username' => USERNAME,
+        'password' => PASSWORD
+    ]);
+    $request = new Request(
+        'POST',
+        'http://shopware.p574147.webspaceconfig.de/api/oauth/token',
+        ['Content-Type' => 'application/json'],
+        $body
+    );
+    $response = $client->send($request);
+    $body = json_decode($response->getBody()->getContents(), true);
+    $accessToken = $body['access_token'];
+    $refreshToken = $body['refresh_token'];
+    return $accessToken;
+}
+
+$request = new Request(
+    'PATCH',
+    HOST . '/api/order-delivery/2f41a90c2fe943f6829cab2e8d6e0b42',
+    [
+        'Authorization' => 'Bearer ' . $accessToken,
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+    ],
+    json_encode([
+        'id' => '2f41a90c2fe943f6829cab2e8d6e0b42',
+        'trackingCodes' => [(string)rand(111111, 999999)]
+    ])
+);
+$response = $client->send($request);
+var_dump($body['data']);
+$body = json_decode($response->getBody()->getContents(), true);
