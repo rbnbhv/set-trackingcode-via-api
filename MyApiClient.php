c6e3330342d4ab0968616e4411d5413de59942bf
Ruben Allenstein <ru.allenstein@reply.de>
2022-12-09 11:19:07 +0100

    [UPDATE] updating trackingcode id

---
+++ MyApiClient.php
@@ -0,0 +1,90 @@
+<?php
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+
+class MyApiClient
+{
+    const HOST = 'http://shopware.p574147.webspaceconfig.de';
+    private Client $client;
+    private string $accessToken;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+        $this->getAccessToken();
+    }
+
+    public function getAccessToken() : void
+    {
+        $body = json_encode([
+            'client_id' => 'administration',
+            'grant_type' => 'password',
+            'scopes' => '',
+            'username' => USERNAME,
+            'password' => PASSWORD
+        ]);
+        $request = new Request(
+            'POST',
+            self::HOST.'/api/oauth/token',
+            ['Content-Type' => 'application/json'],
+            $body
+        );
+        $response = $this->sendRequest($request);
+
+        $body = json_decode($response->getBody()->getContents(), true);
+        $this->accessToken = $body['access_token'];
+    }
+
+    public function setTrackingcode(string $id, array $trackingCodes) : void
+    {
+        $request = new Request(
+            'PATCH',
+            self::HOST . '/api/order-delivery/' . $id,
+            [
+                'Authorization' => 'Bearer ' . $this->accessToken,
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+            ],
+            json_encode([
+                'id' => '2f41a90c2fe943f6829cab2e8d6e0b42',
+                'trackingCodes' => $trackingCodes
+            ])
+        );
+        $this->sendRequest($request);
+    }
+
+    public function markOrderAsShipped(string $id) : void
+    {
+        $request = new Request(
+            'POST',
+            self::HOST . '/api/_action/order_delivery/' . $id . '/state/ship',
+            [
+                'Authorization' => 'Bearer ' . $this->accessToken,
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+            ],
+            json_encode([
+                'sendMail' => false,
+                'documentIds' => [
+                ],
+                'mediaIds' => [
+                ],
+                'stateFieldName' => 'stateId'
+            ])
+        );
+        $this->sendRequest($request);
+    }
+    private function sendRequest($request) : Response | null  {
+        $response = null;
+        try {
+            $response = $this->client->send($request);
+        }
+        catch(GuzzleException $exception){
+            echo $exception->getMessage();
+        }
+        return $response;
+    }
+}
--- setTrackingcode.php
+++ setTrackingcode.php
@@ -1,78 +1,13 @@
 <?php
 
-
 include 'Credentials.php';
+include 'MyApiClient.php';
 require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
 
+$orderId = '2f41a90c2fe943f6829cab2e8d6e0b42';
 $client = new Client();
-$accessToken = getAccessToken($client);
-
-$request = new Request(
-    'GET',
-    HOST . '/api/order/6e96bb30203e4355b6a10caeec40c1b3',
-    [
-        'Authorization' => 'Bearer ' . $accessToken,
-        'Accept' => 'application/json',
-        'Content-Type' => 'application/json'
-    ]
-);
-$response = $client->send($request);
-$body = json_decode($response->getBody()->getContents(), true);
-
-function getAccessToken($client)
-{
-    $body = json_encode([
-        'client_id' => 'administration',
-        'grant_type' => 'password',
-        'scopes' => '',
-        'username' => USERNAME,
-        'password' => PASSWORD
-    ]);
-    $request = new Request(
-        'POST',
-        'http://shopware.p574147.webspaceconfig.de/api/oauth/token',
-        ['Content-Type' => 'application/json'],
-        $body
-    );
-    $response = $client->send($request);
-    $body = json_decode($response->getBody()->getContents(), true);
-    $accessToken = $body['access_token'];
-    $refreshToken = $body['refresh_token'];
-    return $accessToken;
-}
-
-$request = new Request(
-    'PATCH',
-    HOST . '/api/order-delivery/2f41a90c2fe943f6829cab2e8d6e0b42',
-    [
-        'Authorization' => 'Bearer ' . $accessToken,
-        'Accept' => 'application/json',
-        'Content-Type' => 'application/json'
-    ],
-    json_encode([
-        'id' => '2f41a90c2fe943f6829cab2e8d6e0b42',
-        'trackingCodes' => ['123456789']
-    ])
-);
-$response = $client->send($request);
-var_dump($body['data']);
-$body = json_decode($response->getBody()->getContents(), true);
-
-$request = new Request(
-    'POST',
-    HOST . '/api/_action/order_delivery/2f41a90c2fe943f6829cab2e8d6e0b42/state/ship',
-    [
-        'Authorization' => 'Bearer ' . $accessToken,
-        'Accept' => 'application/json',
-        'Content-Type' => 'application/json'
-    ],
-    json_encode([
-        'documentIds' => [],
-        'sendMail' => true
-    ])
-);
-$response = $client->send($request);
-$body = json_decode($response->getBody()->getContents(), true);
+$apiClient = new MyApiClient($client);
+$apiClient->setTrackingcode($orderId, ['123456789']);
+$apiClient->markOrderAsShipped($orderId);
